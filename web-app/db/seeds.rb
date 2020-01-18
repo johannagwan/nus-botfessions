0cf4db5ce55233b86c8825273586850302e78477
Christian James Welly <chrisjwelly@gmail.com>
2020-01-18 22:36:29 +0800

    Add seed for confessions

--- web-app/db/seeds.rb
+++ web-app/db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+9.times do |i|
+  Confession.create(
+    body: "This is confession #{i + 1}",
+    category: rand(1..10)
+  )
+end
