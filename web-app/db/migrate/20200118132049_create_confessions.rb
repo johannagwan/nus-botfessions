43a1617d416d5124060db8c9d665fca10fd6ab9b
Christian James Welly <chrisjwelly@gmail.com>
2020-01-18 21:32:41 +0800

    Add migration and correct int to integer

--- web-app/db/migrate/20200118132049_create_confessions.rb
+++ web-app/db/migrate/20200118132049_create_confessions.rb
@@ -1,8 +1,8 @@
 class CreateConfessions < ActiveRecord::Migration[5.2]
   def change
     create_table :confessions do |t|
-      t.text :body
-      t.int :category
+      t.text :body, null: false
+      t.integer :category, null: false
 
       t.timestamps
     end
