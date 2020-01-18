b45b0eb8b62eb852b669dec5898a1ef3e8bd2c62
Christian James Welly <chrisjwelly@gmail.com>
2020-01-18 21:50:21 +0800

    Update confessions_controller.rb

--- web-app/app/controllers/confessions_controller.rb
+++ web-app/app/controllers/confessions_controller.rb
@@ -1,10 +1,33 @@
 class ConfessionsController < ApplicationController
   def index
+    confession = Confession.all.order(created_at: :desc)
+    render json: confession
   end
 
   def create
+    confession = Confession.create!(confession_params)
+    if confession
+      render json: confession
+    else
+      render json: confession.errors
+    end
   end
 
   def show
+    if confession
+      render json: confession
+    else
+      render json: confession.errors
+    end
+  end
+
+  private
+
+  def confession_params
+    params.permit(:body, :category)
+  end
+
+  def confession
+    @confession ||= Confession.find(params[:id])
   end
 end
