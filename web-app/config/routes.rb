247234e4cfab877a0779ba4874f3c7a7f590e75a
Christian James Welly <chrisjwelly@gmail.com>
2020-01-18 21:53:26 +0800

    Correct recipe to confessions

--- web-app/config/routes.rb
+++ web-app/config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   get 'confessions/index'
   post 'confessions/create'
-  get '/show/:id', to: 'recipes#show'
+  get '/show/:id', to: 'confessions#show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
